de874a92eb0c68ecf1caa6580e31844f73b8c076
Mika-Matti K Karikko <karikko@svm-26.cs.helsinki.fi>
2015-03-19 16:08:10 +0200

    /app/view/suunnitelmat kansio luotu

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,25 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make ('helloworld.html');
     }
+
+    public static function drink_list(){
+    View::make('suunnitelmat/drink_list.html');
+    }
+
+    public static function drink_show(){
+    View::make('suunnitelmat/drink_show.html');
+    }
+  
+    public static function drink_edit(){
+    View::make('suunnitelmat/drink_edit.html');
+    }
+
+
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,16 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/drink', function() {
+    HelloWorldController::drink_list();
+  });
+
+  $routes->get('/drink/1', function() {
+    HelloWorldController::drink_show();
+  });
+
+  $routes->get('/edit', function() {
+    HelloWorldController::drink_edit();
+  });
+
